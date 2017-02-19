2fce005fc7c289bbd3eb80ff67d57b9a9b92e7ac
Michael Bowe <michael_bowe@student.uml.edu>
2017-02-18 22:45:08 -0500

    Log connections to screen

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket
+import json
+
+configs = {"ip":"10.101.44.12", "listen_port":2048, "boradcast_port":4095}
 
 def init():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((socket.gethostname(), 2048))
+    print(serversocket)
+    serversocket.bind((configs['ip'], config["listen_port"]))
+    print(serversocket)
     serversocket.listen(10)
     while 1:
         (clientsocket, address) = serversocket.accept()
         # Do something
+        print(address)
 init()
